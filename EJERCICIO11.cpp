74cd81f9a19d37e518e20a118027767e47f8f362
Jose <jvilcanquic@unjbg.edu.pe>
2024-06-18 23:10:58 -0500

    Se agrego una libreria

--- EJERCICIO11.cpp
+++ EJERCICIO11.cpp
@@ -1,24 +1,28 @@
 /*EJERCICIO 11: Leer 3 números q, r y s. Determine si q es multiplo de r y s.*/
 #include <iostream>
+#include <windows.h>
+
 using namespace std;
 int main(){
-	int q,r,s;
 	
-	cout<<"Escribe el primer numero: "<<endl;
+	int q,r,s;
+	SetConsoleOutputCP(CP_UTF8);
+
+	cout<<"Escribe el primer número: "<<endl;
 	cin>>q;
-	cout<<"Escribe el segundo numero: "<<endl;
+	cout<<"Escribe el segundo número: "<<endl;
 	cin>>r;
-	cout<<"Escribe el tercer numero: "<<endl;
+	cout<<"Escribe el tercer número: "<<endl;
 	cin>>s;
 	
 	if(q%r==0){
 		if(q%s==0){
-			cout<<"El primer numero es multiplo del segundo y tercero"<<endl;
+			cout<<"El primer número es múltiplo del segundo y tercero"<<endl;
 		}else{
-			cout<<"El primer numero es multiplo del segundo pero no del  tercero"<<endl;
+			cout<<"El primer número es múltiplo del segundo pero no del  tercero"<<endl;
 		}
 	}else{
-		cout<<"El primer numero no es multiplo del segundo"<<endl;
+		cout<<"El primer número no es múltiplo del segundo"<<endl;
 	}
 	return 0;
 }
